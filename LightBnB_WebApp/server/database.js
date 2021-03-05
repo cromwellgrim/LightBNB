3480e0302b181ba8df735b7130a0eda98810f2c2
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-05 01:03:03 +0000

    getAllReservations now uses an SQL query to find a user's reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -71,7 +71,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::DATE
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2
+  `;
+  return pool.query(queryString, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(err => console.log(err.stack));
 }
 exports.getAllReservations = getAllReservations;
 
